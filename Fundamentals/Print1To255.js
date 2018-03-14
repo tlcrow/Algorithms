c0a4e4e7a282a8f5aca5f6fe44be8e4b1a6b4b4b
TLC <pi_lover852@yahoo.com>
2018-03-14 14:47:00 -0600

    Algorithm Fundamentals

---
+++ Fundamentals/Print1To255.js
@@ -0,0 +1,10 @@
+//Print all the integers from 1 to 255
+
+function print1to255(){
+    var num = 1;
+    while(num <= 255){
+        console.log(num);
+        num = num + 1;
+    }
+}
+print1to255();
