940334ab564f774f8e31e03f48bcb79f00eebfc8
TLC <pi_lover852@yahoo.com>
2018-03-25 13:45:52 -0600

    Algorithm Fundamentals

---
+++ Fundamentals/StarArt.js
@@ -0,0 +1,16 @@
+//Assume that you have a text field that is exactly 75 characters long. You want to fill it with spaces and asterisks, sometimes called stars.  You should print the given number of stars consecutively.  Depending on which function is called, those stars should be left-justified, right-justified, or centered (plus/minus one). Write a function that accepts a number and prints that many asterisks. Write a function that prints 75 characters total. Stars should build from the right side. Write function that prints 75 characters total.  the stars should be centered in the 75.
+
+function drawLeftStars(num){
+    var textfield = '';
+    while(num < 0){
+
+    }
+}
+
+function drawRightStars(num){
+
+}
+
+function drawCenteredStars(num){
+
+}
