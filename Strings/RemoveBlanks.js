cb1cc09f9fa9a0365ffda3e795a670ec7ae92a7f
TLC <pi_lover852@yahoo.com>
2018-04-03 13:32:48 -0600

    In progress

---
+++ Strings/RemoveBlanks.js
@@ -0,0 +1,6 @@
+//Create a function that, given a string, returns all of that string's contents. but without blanks.
+
+function RemoveBlanks(string){
+
+}
+RemoveBlanks(" Pl   ayTha  tF  u  nkyM usi   c  ")
