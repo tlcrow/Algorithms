c339947f3368dc236d0598d8f7d2d8ba5484eb67
TLC <pi_lover852@yahoo.com>
2018-04-03 15:18:37 -0600

    String Basics

--- Strings/RemoveBlanks.js
+++ Strings/RemoveBlanks.js
@@ -1,6 +1,16 @@
 //Create a function that, given a string, returns all of that string's contents. but without blanks.
 
-function RemoveBlanks(string){
 
+//Sovled by creating new array
+function RemoveBlanks(string){
+    var newString = "";
+    for(var i = 0; i < string.length; i++){
+        if(string[i] != " "){
+            newString = newString + string[i];
+        }
+    }
+    console.log(newString);
+    return(newString);
 }
-RemoveBlanks(" Pl   ayTha  tF  u  nkyM usi   c  ")
+
+RemoveBlanks(" Pl   ayTha  tF  u  nkyM usi   c  ");
