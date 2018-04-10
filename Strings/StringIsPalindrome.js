d2918a66d63df64f247fb4dd50a387f40790e710
TLC <pi_lover852@yahoo.com>
2018-04-09 20:38:48 -0600

    String Comparisons

--- Strings/StringIsPalindrome.js
+++ Strings/StringIsPalindrome.js
@@ -10,7 +10,34 @@ function StrictPalindrome(str){
     console.log(true);
     return(true);
 }
+
+//Do ignore white space (spaces, tabs) and capitalization 
+function IsPalindrome(str){
+    str = str.toLowerCase();
+    for(var i = 0, j = str.length - 1; i <= j; i++, j--){
+        if(str[i] == " " ||
+           str[i] == "  "){
+               j = j + 1;
+               continue;
+           }
+        if(str[j] == " " ||
+           str[j] == "  "){
+               i = i - 1;
+               continue;
+           }
+        else if(str[i] != str[j]){
+            console.log(false);
+            return(false);
+        }
+    }
+    console.log(true);
+    return(true);
+}
 StrictPalindrome("a x a");
 StrictPalindrome("racecar");
 StrictPalindrome("Dud");
 StrictPalindrome("oho!");
+IsPalindrome("Ra ce car");
+IsPalindrome("a x a");
+IsPalindrome("TaCo CaT");
+IsPalindrome("Tao CaT");
