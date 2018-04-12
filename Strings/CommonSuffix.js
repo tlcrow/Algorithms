db0620670f18fec708c5f8014d33167b88fb00ff
TLC <pi_lover852@yahoo.com>
2018-04-12 16:36:01 -0600

    String Comparisons

--- Strings/CommonSuffix.js
+++ Strings/CommonSuffix.js
@@ -2,15 +2,27 @@
 
 function CommonSuffix(arr){
     var suffix = "";
+    var compare = "";
     for(var i = 0; i < arr.length - 1; i++){
         for(var j = arr[i].length - 1, k = arr[i + 1].length - 1; j >= 0, k >= 0; j--, k--){
-            console.log(arr[i][j] + " " + arr[i + 1][k])
-            if(arr[i][j] == arr[i + 1][k]){
+            if(i == 0 && arr[i][j] == arr[i + 1][k]){
+                compare = compare + arr[i][j];
+            }
+            else if(arr[i][j] == arr[i + 1][k]){
                 suffix = suffix + arr[i][j];
             }
         }
+        if(i != 0 && suffix.length < compare.length){
+            compare = suffix;
+            suffix = "";
+        }
+        suffix = ""
+    }
+    for(var m = compare.length - 1; m >= 0; m--){
+        suffix = suffix + compare[m];
     }
     console.log(suffix);
 }
 CommonSuffix(["deforestation", "citation", "conviction", "incarceration"]);
-// CommonSuffix(["nice", "ice", "baby"]);
+CommonSuffix(["nice", "ice", "baby"]);
+CommonSuffix(["fedcba", "ghuba", "tunwa"]);
