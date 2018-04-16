c0937a9d2be2ce606d79ae55190b8ce530a0db54
TLC <pi_lover852@yahoo.com>
2018-04-16 14:28:46 -0600

    Hashing Basics

---
+++ Hashes/MaxMinAverageWithObject.js
@@ -0,0 +1,20 @@
+//Given an array, return an object containing the array's max, min, and average values.
+
+function MaxMinAverageWithObjects(arr){
+    var myAssoc = {max: arr[0], min: arr[0]};
+    var sum = 0;
+
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] > myAssoc.max){
+            myAssoc.max = arr[i];
+        }
+        if(arr[i] < myAssoc.min){
+            myAssoc.min = arr[i];
+        }
+        sum = sum + arr[i];
+    }
+    myAssoc.average = sum / arr.length;
+    console.log(myAssoc);
+}
+MaxMinAverageWithObjects([1,2,3,4,5]);
+MaxMinAverageWithObjects([4, 80, 23, 1]);
